111274ebbdc926fdd14b8b9cde62f54dffa5cef2
JJB <131276124+Jhonnyjb02@users.noreply.github.com>
2025-03-17 14:19:05 -0400

    Add files via upload

---
+++ One_Piece.js
@@ -0,0 +1,503 @@
+[
+    {
+      "id": 1,
+      "name": "Monkey D Luffy",
+      "size": "174cm",
+      "age": "19 años",
+      "bounty": "3.000.000.000",
+      "crew": {
+        "id": 1,
+        "name": "La tripulación del Sombrero de Paja",
+        "description": "Tripulación liderada por Monkey D. Luffy",
+        "status": "activo",
+        "number": "1",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": 1,
+        "name": "Fruta Gomu Gomu",
+        "description": "La fruta Gomu Gomu, también conocida como la Fruta de la Goma-Goma, es una Fruta del Diablo de tipo Paramecia que otorga propiedades de caucho al cuerpo, convirtiendo a su usuario en un Hombre Elástico. Fue un tesoro obtenido de un convoy del Gobierno Mundial, protegido por CP9, que terminó siendo consumido accidentalmente por Luffy.",
+        "type": "Paramecia",
+        "filename": "https://images.api-onepiece.com/fruits/5665e89442022d4c0e7684c650dc6d6b.png",
+        "technicalFile": "https://images.api-onepiece.com/fruits/"
+      },
+      "job": "Capitán",
+      "status": "vivo"
+    },
+    {
+      "id": 2,
+      "name": "Roronoa Zoro",
+      "size": "181cm",
+      "age": "21 años",
+      "bounty": "1.000.000.000",
+      "crew": {
+        "id": 1,
+        "name": "La tripulación del Sombrero de Paja",
+        "description": "Tripulación liderada por Monkey D. Luffy",
+        "status": "activo",
+        "number": "2",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": null,
+        "name": null,
+        "description": null,
+        "type": null,
+        "filename": null,
+        "technicalFile": null
+      },
+      "job": "Espadachín",
+      "status": "vivo"
+    },
+    {
+      "id": 3,
+      "name": "Nami",
+      "size": "170cm",
+      "age": "20 años",
+      "bounty": "366.000.000",
+      "crew": {
+        "id": 1,
+        "name": "La tripulación del Sombrero de Paja",
+        "description": "Tripulación liderada por Monkey D. Luffy",
+        "status": "activo",
+        "number": "3",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": null,
+        "name": null,
+        "description": null,
+        "type": null,
+        "filename": null,
+        "technicalFile": null
+      },
+      "job": "Navegante",
+      "status": "vivo"
+    },
+    {
+      "id": 4,
+      "name": "Usopp",
+      "size": "176cm",
+      "age": "19 años",
+      "bounty": "200.000.000",
+      "crew": {
+        "id": 1,
+        "name": "La tripulación del Sombrero de Paja",
+        "description": "Tripulación liderada por Monkey D. Luffy",
+        "status": "activo",
+        "number": "4",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": null,
+        "name": null,
+        "description": null,
+        "type": null,
+        "filename": null,
+        "technicalFile": null
+      },
+      "job": "Francotirador",
+      "status": "vivo"
+    },
+    {
+      "id": 5,
+      "name": "Sanji",
+      "size": "180cm",
+      "age": "21 años",
+      "bounty": "1.032.000.000",
+      "crew": {
+        "id": 1,
+        "name": "La tripulación del Sombrero de Paja",
+        "description": "Tripulación liderada por Monkey D. Luffy",
+        "status": "activo",
+        "number": "5",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": null,
+        "name": null,
+        "description": null,
+        "type": null,
+        "filename": null,
+        "technicalFile": null
+      },
+      "job": "Cocinero",
+      "status": "vivo"
+    },
+    {
+      "id": 6,
+      "name": "Tony Tony Chopper",
+      "size": "90cm",
+      "age": "15 años",
+      "bounty": "0",
+      "crew": {
+        "id": 1,
+        "name": "La tripulación del Sombrero de Paja",
+        "description": "Tripulación liderada por Monkey D. Luffy",
+        "status": "activo",
+        "number": "6",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": 2,
+        "name": "Fruta Hito Hito",
+        "description": "La Fruta Hito Hito permite al usuario transformarse en humano o en híbrido, otorgándole inteligencia y características humanas.",
+        "type": "Zoan",
+        "filename": "https://images.api-onepiece.com/fruits/hito-hito-no-mi.png",
+        "technicalFile": "https://images.api-onepiece.com/fruits/"
+      },
+      "job": "Doctor",
+      "status": "vivo"
+    },
+    {
+      "id": 7,
+      "name": "Nico Robin",
+      "size": "188cm",
+      "age": "30 años",
+      "bounty": "800.000.000",
+      "crew": {
+        "id": 1,
+        "name": "La tripulación del Sombrero de Paja",
+        "description": "Tripulación liderada por Monkey D. Luffy",
+        "status": "activo",
+        "number": "7",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": 3,
+        "name": "Fruta Hana Hana",
+        "description": "La Fruta Hana Hana permite a su usuaria manifestar partes de su cuerpo en cualquier superficie a voluntad.",
+        "type": "Paramecia",
+        "filename": "https://images.api-onepiece.com/fruits/hana-hana-no-mi.png",
+        "technicalFile": "https://images.api-onepiece.com/fruits/"
+      },
+      "job": "Arqueóloga",
+      "status": "vivo"
+    },
+    {
+      "id": 8,
+      "name": "Franky",
+      "size": "240cm",
+      "age": "36 años",
+      "bounty": "940.000.000",
+      "crew": {
+        "id": 1,
+        "name": "La tripulación del Sombrero de Paja",
+        "description": "Tripulación liderada por Monkey D. Luffy",
+        "status": "activo",
+        "number": "8",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": null,
+        "name": null,
+        "description": null,
+        "type": null,
+        "filename": null,
+        "technicalFile": null
+      },
+      "job": "Carpintero",
+      "status": "vivo"
+    },
+    {
+      "id": 9,
+      "name": "Brook",
+      "size": "277cm",
+      "age": "90 años",
+      "bounty": "83.000.000",
+      "crew": {
+        "id": 1,
+        "name": "La tripulación del Sombrero de Paja",
+        "description": "Tripulación liderada por Monkey D. Luffy",
+        "status": "activo",
+        "number": "9",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": 4,
+        "name": "Fruta Yomi Yomi",
+        "description": "La Fruta Yomi Yomi permite a su usuario revivir tras la muerte, otorgándole una segunda oportunidad en la vida.",
+        "type": "Paramecia",
+        "filename": "https://images.api-onepiece.com/fruits/yomi-yomi-no-mi.png",
+        "technicalFile": "https://images.api-onepiece.com/fruits/"
+      },
+      "job": "Músico",
+      "status": "vivo"
+    },
+    {
+      "id": 10,
+      "name": "Jinbe",
+      "size": "300cm",
+      "age": "60 años",
+      "bounty": "438.000.000",
+      "crew": {
+        "id": 1,
+        "name": "La tripulación del Sombrero de Paja",
+        "description": "Tripulación liderada por Monkey D. Luffy",
+        "status": "activo",
+        "number": "10",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": null,
+        "name": null,
+        "description": null,
+        "type": null,
+        "filename": null,
+        "technicalFile": null
+      },
+      "job": "Timonel",
+      "status": "vivo"
+    },
+    {
+      "id": 11,
+      "name": "Portgas D. Ace",
+      "size": "185cm",
+      "age": "20 años",
+      "bounty": "550.000.000",
+      "crew": {
+        "id": 2,
+        "name": "Piratas de Barbablanca",
+        "description": "Piratas liderados por Edward Newgate (Barbablanca)",
+        "status": "activo",
+        "number": "2",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": null,
+        "name": null,
+        "description": null,
+        "type": null,
+        "filename": null,
+        "technicalFile": null
+      },
+      "job": "Comandante de la segunda división",
+      "status": "muerto"
+    },
+    {
+      "id": 12,
+      "name": "Trafalgar D. Law",
+      "size": "188cm",
+      "age": "32 años",
+      "bounty": "500.000.000",
+      "crew": {
+        "id": 3,
+        "name": "Piratas del Corazón",
+        "description": "Tripulación liderada por Trafalgar Law",
+        "status": "activo",
+        "number": "1",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": 5,
+        "name": "Fruta Ope Ope",
+        "description": "La Fruta Ope Ope permite al usuario crear una esfera de control en la que puede realizar milagros quirúrgicos.",
+        "type": "Paramecia",
+        "filename": "https://images.api-onepiece.com/fruits/ope-ope-no-mi.png",
+        "technicalFile": "https://images.api-onepiece.com/fruits/"
+      },
+      "job": "Capitán",
+      "status": "vivo"
+    },
+    {
+      "id": 13,
+      "name": "Marshall D. Teach (Blackbeard)",
+      "size": "275cm",
+      "age": "desconocido",
+      "bounty": "2.247.600.000",
+      "crew": {
+        "id": 4,
+        "name": "Piratas de Barbanegra",
+        "description": "Tripulación liderada por Marshall D. Teach",
+        "status": "activo",
+        "number": "1",
+        "is_yonko": true
+      },
+      "fruit": {
+        "id": 6,
+        "name": "Fruta Yami Yami",
+        "description": "La Fruta Yami Yami permite al usuario manipular la gravedad y la oscuridad, absorbiendo y aniquilando a sus oponentes.",
+        "type": "Logia",
+        "filename": "https://images.api-onepiece.com/fruits/yami-yami-no-mi.png",
+        "technicalFile": "https://images.api-onepiece.com/fruits/"
+      },
+      "job": "Capitán",
+      "status": "vivo"
+    },
+    {
+      "id": 14,
+      "name": "Shanks",
+      "size": "185cm",
+      "age": "45 años",
+      "bounty": "desconocido",
+      "crew": {
+        "id": 5,
+        "name": "Piratas de Cabello Rojo",
+        "description": "Tripulación liderada por Shanks",
+        "status": "activo",
+        "number": "1",
+        "is_yonko": true
+      },
+      "fruit": {
+        "id": null,
+        "name": null,
+        "description": null,
+        "type": null,
+        "filename": null,
+        "technicalFile": null
+      },
+      "job": "Capitán",
+      "status": "vivo"
+    },
+    {
+      "id": 15,
+      "name": "Boa Hancock",
+      "size": "188cm",
+      "age": "31 años",
+      "bounty": "80.000.000",
+      "crew": {
+        "id": 6,
+        "name": "Piratas Kuja",
+        "description": "Tripulación liderada por Boa Hancock",
+        "status": "activo",
+        "number": "1",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": null,
+        "name": null,
+        "description": null,
+        "type": null,
+        "filename": null,
+        "technicalFile": null
+      },
+      "job": "Capitana",
+      "status": "vivo"
+    },
+    {
+      "id": 16,
+      "name": "Dracule Mihawk",
+      "size": "190cm",
+      "age": "50 años",
+      "bounty": "desconocido",
+      "crew": {
+        "id": 7,
+        "name": "Independiente",
+        "description": "No pertenece a una tripulación",
+        "status": "activo",
+        "number": "1",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": null,
+        "name": null,
+        "description": null,
+        "type": null,
+        "filename": null,
+        "technicalFile": null
+      },
+      "job": "Espadachín",
+      "status": "vivo"
+    },
+    {
+      "id": 17,
+      "name": "Donquixote Doflamingo",
+      "size": "191cm",
+      "age": "46 años",
+      "bounty": "340.000.000",
+      "crew": {
+        "id": 8,
+        "name": "Piratas Donquixote",
+        "description": "Tripulación liderada por Donquixote Doflamingo",
+        "status": "activo",
+        "number": "1",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": 7,
+        "name": "Fruta Ito Ito",
+        "description": "La Fruta Ito Ito permite al usuario crear y manipular hilos para controlar a sus oponentes.",
+        "type": "Paramecia",
+        "filename": "https://images.api-onepiece.com/fruits/ito-ito-no-mi.png",
+        "technicalFile": "https://images.api-onepiece.com/fruits/"
+      },
+      "job": "Capitán",
+      "status": "vivo"
+    },
+    {
+      "id": 18,
+      "name": "Sabo",
+      "size": "192cm",
+      "age": "25 años",
+      "bounty": "0",
+      "crew": {
+        "id": 9,
+        "name": "Ejército Revolucionario",
+        "description": "Organización revolucionaria contra el Gobierno Mundial",
+        "status": "activo",
+        "number": "1",
+        "is_yonko": false
+      },
+      "fruit": {
+        "id": 8,
+        "name": "Fruta Mera Mera",
+        "description": "La Fruta Mera Mera permite al usuario controlar el fuego y transformarse en llamas.",
+        "type": "Logia",
+        "filename": "https://images.api-onepiece.com/fruits/mera-mera-no-mi.png",
+        "technicalFile": "https://images.api-onepiece.com/fruits/"
+      },
+      "job": "Líder revolucionario",
+      "status": "vivo"
+    },
+    {
+      "id": 19,
+      "name": "Kaido",
+      "size": "600cm",
+      "age": "desconocido",
+      "bounty": "4.611.100.000",
+      "crew": {
+        "id": 10,
+        "name": "Piratas de las Bestias",
+        "description": "Tripulación liderada por Kaido",
+        "status": "activo",
+        "number": "1",
+        "is_yonko": true
+      },
+      "fruit": {
+        "id": 9,
+        "name": "Fruta Uo Uo, Modelo Seiryu",
+        "description": "La Fruta Uo Uo, Modelo Seiryu permite al usuario transformarse en un dragón, otorgándole habilidades dracónicas.",
+        "type": "Zoan (Mítica)",
+        "filename": "https://images.api-onepiece.com/fruits/uo-uo-modelo-seiryu.png",
+        "technicalFile": "https://images.api-onepiece.com/fruits/"
+      },
+      "job": "Capitán",
+      "status": "vivo"
+    },
+    {
+      "id": 20,
+      "name": "Charlotte Linlin (Big Mom)",
+      "size": "850cm",
+      "age": "68 años",
+      "bounty": "4.388.000.000",
+      "crew": {
+        "id": 11,
+        "name": "Tripulación de Big Mom",
+        "description": "Tripulación liderada por Charlotte Linlin (Big Mom)",
+        "status": "activo",
+        "number": "1",
+        "is_yonko": true
+      },
+      "fruit": {
+        "id": 10,
+        "name": "Fruta Soru Soru",
+        "description": "La Fruta Soru Soru permite a su usuaria manipular y extraer las almas, otorgándole el poder de controlar el destino de seres vivos y objetos.",
+        "type": "Paramecia",
+        "filename": "https://images.api-onepiece.com/fruits/soru-soru-no-mi.png",
+        "technicalFile": "https://images.api-onepiece.com/fruits/"
+      },
+      "job": "Capitana",
+      "status": "vivo"
+    }
+  ]
+  
