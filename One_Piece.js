aa58860f77298eebed5b2a07dceacd5e8f803f17
JJB <131276124+Jhonnyjb02@users.noreply.github.com>
2025-03-17 20:57:12 -0400

    Update One_Piece.js

--- One_Piece.js
+++ One_Piece.js
@@ -18,7 +18,7 @@
         "name": "Fruta Gomu Gomu",
         "description": "La fruta Gomu Gomu, también conocida como la Fruta de la Goma-Goma, es una Fruta del Diablo de tipo Paramecia que otorga propiedades de caucho al cuerpo, convirtiendo a su usuario en un Hombre Elástico. Fue un tesoro obtenido de un convoy del Gobierno Mundial, protegido por CP9, que terminó siendo consumido accidentalmente por Luffy.",
         "type": "Paramecia",
-        "filename": "https://images.api-onepiece.com/fruits/5665e89442022d4c0e7684c650dc6d6b.png",
+        "filename": "https://scc10.com.br/wp-content/uploads/2024/10/luffy.webp",
         "technicalFile": "https://images.api-onepiece.com/fruits/"
       },
       "job": "Capitán",
@@ -43,7 +43,7 @@
         "name": null,
         "description": null,
         "type": null,
-        "filename": null,
+        "filename":"https://asset-2.tstatic.net/tribunnewswiki/foto/bank/images/Teori-One-Piece-Terungkap-Alasan-Roronoa-Zoro-Bisa-Membuka-Mata-Kirinya-di-Elbaf.jpg",
         "technicalFile": null
       },
       "job": "Espadachín",
@@ -68,7 +68,7 @@
         "name": null,
         "description": null,
         "type": null,
-        "filename": null,
+        "filename": "https://i.blogs.es/971694/nami/1366_2000.jpeg",
         "technicalFile": null
       },
       "job": "Navegante",
@@ -93,7 +93,7 @@
         "name": null,
         "description": null,
         "type": null,
-        "filename": null,
+        "filename": "https://pm1.aminoapps.com/6420/fbd02c7d3c06a074433a34639f8ac31ee0035e56_hq.jpg",
         "technicalFile": null
       },
       "job": "Francotirador",
@@ -118,7 +118,7 @@
         "name": null,
         "description": null,
         "type": null,
-        "filename": null,
+        "filename": "https://spellmana.com/wp-content/uploads/2024/08/red-sanji-deck-guide.webp",
         "technicalFile": null
       },
       "job": "Cocinero",
@@ -143,7 +143,7 @@
         "name": "Fruta Hito Hito",
         "description": "La Fruta Hito Hito permite al usuario transformarse en humano o en híbrido, otorgándole inteligencia y características humanas.",
         "type": "Zoan",
-        "filename": "https://images.api-onepiece.com/fruits/hito-hito-no-mi.png",
+        "filename": "https://upload.wikimedia.org/wikipedia/en/f/f6/Tony_Tony_Chopper.jpg",
         "technicalFile": "https://images.api-onepiece.com/fruits/"
       },
       "job": "Doctor",
@@ -168,7 +168,7 @@
         "name": "Fruta Hana Hana",
         "description": "La Fruta Hana Hana permite a su usuaria manifestar partes de su cuerpo en cualquier superficie a voluntad.",
         "type": "Paramecia",
-        "filename": "https://images.api-onepiece.com/fruits/hana-hana-no-mi.png",
+        "filename": "https://jetex.id/blog/wp-content/uploads/2024/10/image1-6.jpg",
         "technicalFile": "https://images.api-onepiece.com/fruits/"
       },
       "job": "Arqueóloga",
@@ -193,7 +193,7 @@
         "name": null,
         "description": null,
         "type": null,
-        "filename": null,
+        "filename": "https://cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/2BTBFCBYCRFWTCE3LWWMYANBKE.jpg",
         "technicalFile": null
       },
       "job": "Carpintero",
@@ -218,7 +218,7 @@
         "name": "Fruta Yomi Yomi",
         "description": "La Fruta Yomi Yomi permite a su usuario revivir tras la muerte, otorgándole una segunda oportunidad en la vida.",
         "type": "Paramecia",
-        "filename": "https://images.api-onepiece.com/fruits/yomi-yomi-no-mi.png",
+        "filename": "https://a.storyblok.com/f/178900/960x540/e7b14b2dff/brook-one-piece.jpg/m/filters:quality(95)format(webp)",
         "technicalFile": "https://images.api-onepiece.com/fruits/"
       },
       "job": "Músico",
@@ -243,7 +243,7 @@
         "name": null,
         "description": null,
         "type": null,
-        "filename": null,
+        "filename": "https://content.emarket.pe/common/collections/content/67/e7/67e74ff8-e0b1-43e1-958b-16f551bb990a.jpg",
         "technicalFile": null
       },
       "job": "Timonel",
@@ -268,7 +268,7 @@
         "name": null,
         "description": null,
         "type": null,
-        "filename": null,
+        "filename": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_9QX_o2RVfBR0PhtQ7mlUhAUgCoXtfI6aIA&s",
         "technicalFile": null
       },
       "job": "Comandante de la segunda división",
@@ -293,7 +293,7 @@
         "name": "Fruta Ope Ope",
         "description": "La Fruta Ope Ope permite al usuario crear una esfera de control en la que puede realizar milagros quirúrgicos.",
         "type": "Paramecia",
-        "filename": "https://images.api-onepiece.com/fruits/ope-ope-no-mi.png",
+        "filename": "https://www.proxymitejapon.com/cdn/shop/articles/Law-1.jpg?v=1720437990",
         "technicalFile": "https://images.api-onepiece.com/fruits/"
       },
       "job": "Capitán",
@@ -318,7 +318,7 @@
         "name": "Fruta Yami Yami",
         "description": "La Fruta Yami Yami permite al usuario manipular la gravedad y la oscuridad, absorbiendo y aniquilando a sus oponentes.",
         "type": "Logia",
-        "filename": "https://images.api-onepiece.com/fruits/yami-yami-no-mi.png",
+        "filename": "https://spellmana.com/wp-content/uploads/2024/09/marshal-d-teach-deck-guide-768x432.webp",
         "technicalFile": "https://images.api-onepiece.com/fruits/"
       },
       "job": "Capitán",
@@ -343,7 +343,7 @@
         "name": null,
         "description": null,
         "type": null,
-        "filename": null,
+        "filename": "https://static1.srcdn.com/wordpress/wp-content/uploads/2025/01/shank-and-his-twin.jpg",
         "technicalFile": null
       },
       "job": "Capitán",
@@ -368,7 +368,7 @@
         "name": null,
         "description": null,
         "type": null,
-        "filename": null,
+        "filename": "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/03/X-Best-Femme-Fatales-In-Anime-Boa-Hancock-From-One-Piece-1.jpg",
         "technicalFile": null
       },
       "job": "Capitana",
@@ -393,7 +393,7 @@
         "name": null,
         "description": null,
         "type": null,
-        "filename": null,
+        "filename": "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/04/Mihawk-strength.jpg",
         "technicalFile": null
       },
       "job": "Espadachín",
@@ -418,7 +418,7 @@
         "name": "Fruta Ito Ito",
         "description": "La Fruta Ito Ito permite al usuario crear y manipular hilos para controlar a sus oponentes.",
         "type": "Paramecia",
-        "filename": "https://images.api-onepiece.com/fruits/ito-ito-no-mi.png",
+        "filename": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT906MJ_AkZ9uYadGnzlPoWOcB_H6OQvW6Dhg&s",
         "technicalFile": "https://images.api-onepiece.com/fruits/"
       },
       "job": "Capitán",
@@ -443,7 +443,7 @@
         "name": "Fruta Mera Mera",
         "description": "La Fruta Mera Mera permite al usuario controlar el fuego y transformarse en llamas.",
         "type": "Logia",
-        "filename": "https://images.api-onepiece.com/fruits/mera-mera-no-mi.png",
+        "filename": "https://a.storyblok.com/f/178900/1920x1080/df2aaa51d0/one-piece-episode-1116-sabo.jpg",
         "technicalFile": "https://images.api-onepiece.com/fruits/"
       },
       "job": "Líder revolucionario",
@@ -468,7 +468,7 @@
         "name": "Fruta Uo Uo, Modelo Seiryu",
         "description": "La Fruta Uo Uo, Modelo Seiryu permite al usuario transformarse en un dragón, otorgándole habilidades dracónicas.",
         "type": "Zoan (Mítica)",
-        "filename": "https://images.api-onepiece.com/fruits/uo-uo-modelo-seiryu.png",
+        "filename": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuULytM7A5WAQDp_Jr6qzN5HPjI279xGhu7Q&s",
         "technicalFile": "https://images.api-onepiece.com/fruits/"
       },
       "job": "Capitán",
@@ -493,7 +493,7 @@
         "name": "Fruta Soru Soru",
         "description": "La Fruta Soru Soru permite a su usuaria manipular y extraer las almas, otorgándole el poder de controlar el destino de seres vivos y objetos.",
         "type": "Paramecia",
-        "filename": "https://images.api-onepiece.com/fruits/soru-soru-no-mi.png",
+        "filename": "https://static.wikia.nocookie.net/onepiece/images/6/61/Big_Mom_con_63_a%C3%B1os.png/revision/latest?cb=20171203120140&path-prefix=es",
         "technicalFile": "https://images.api-onepiece.com/fruits/"
       },
       "job": "Capitana",
